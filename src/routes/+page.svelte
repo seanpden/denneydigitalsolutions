23a7a398607c832bb4a6ca6846f6a44748708c51
seanpden <seanden522@gmail.com>
2024-03-29 11:44:08 -0500

    testing changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
-<h1>This is a test</h1>
+<p>This is a test to see if the CI/CD works</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
