fd42632ecf453f5406cc0a27fadb0b4e40e380c8
seanpden <seanden522@gmail.com>
2024-04-11 14:42:15 -0500

    more articles to show

--- src/routes/Blog.svelte
+++ src/routes/Blog.svelte
@@ -44,6 +44,80 @@
 			</article>
 
 			<!-- More posts... -->
+			<article class="flex max-w-xl flex-col items-start justify-between">
+				<div class="flex items-center gap-x-4 text-xs">
+					<time datetime="2020-03-16" class="">2024-04-11</time>
+					<a href="/" class="relative z-10 rounded-full px-3 py-1.5 font-medium">Marketing</a>
+				</div>
+				<div class="group relative">
+					<h3 class="mt-3 text-lg font-semibold leading-6">
+						<a href="/">
+							<span class="absolute inset-0"></span>
+							Boost your conversion rate
+						</a>
+					</h3>
+					<p class="mt-5 line-clamp-3 text-sm leading-6">
+						Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non
+						aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla
+						deserunt vel. Iusto corrupti dicta.
+					</p>
+				</div>
+				<div class="relative mt-8 flex items-center gap-x-4">
+					<img
+						src="https://media.licdn.com/dms/image/C5603AQEmCnECV8F7BA/profile-displayphoto-shrink_400_400/0/1652401479997?e=1718236800&v=beta&t=Iq26PVIfI-pBKzLIsZL0gxSxHTcJnCFvs-K-dPvPw1A"
+						alt=""
+						class="h-10 w-10 rounded-full"
+					/>
+					<div class="text-sm leading-6">
+						<p class="font-semibold">
+							<a href="/">
+								<span class="absolute inset-0"></span>
+								Sean Denney
+							</a>
+						</p>
+						<p class="">Data Engineer</p>
+					</div>
+				</div>
+			</article>
+
+			<!-- More posts... -->
+			<article class="flex max-w-xl flex-col items-start justify-between">
+				<div class="flex items-center gap-x-4 text-xs">
+					<time datetime="2020-03-16" class="">2024-04-11</time>
+					<a href="/" class="relative z-10 rounded-full px-3 py-1.5 font-medium">Marketing</a>
+				</div>
+				<div class="group relative">
+					<h3 class="mt-3 text-lg font-semibold leading-6">
+						<a href="/">
+							<span class="absolute inset-0"></span>
+							Boost your conversion rate
+						</a>
+					</h3>
+					<p class="mt-5 line-clamp-3 text-sm leading-6">
+						Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non
+						aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla
+						deserunt vel. Iusto corrupti dicta.
+					</p>
+				</div>
+				<div class="relative mt-8 flex items-center gap-x-4">
+					<img
+						src="https://media.licdn.com/dms/image/C5603AQEmCnECV8F7BA/profile-displayphoto-shrink_400_400/0/1652401479997?e=1718236800&v=beta&t=Iq26PVIfI-pBKzLIsZL0gxSxHTcJnCFvs-K-dPvPw1A"
+						alt=""
+						class="h-10 w-10 rounded-full"
+					/>
+					<div class="text-sm leading-6">
+						<p class="font-semibold">
+							<a href="/">
+								<span class="absolute inset-0"></span>
+								Sean Denney
+							</a>
+						</p>
+						<p class="">Data Engineer</p>
+					</div>
+				</div>
+			</article>
+
+			<!-- More posts... -->
 		</div>
 	</div>
 </div>
