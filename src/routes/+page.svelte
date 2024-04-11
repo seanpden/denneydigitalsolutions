86aa3330adca4265f73f47c9b8feec5c5222204f
seanpden <seanden522@gmail.com>
2024-04-11 15:15:58 -0500

    contact page fin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,4 +11,4 @@
 
 <Blog />
 
-<!-- <Contact /> -->
+<Contact />
--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -1,1 +1,23 @@
-<h1 id="Contact">Contact</h1>
+<div class=" py-24 sm:py-32">
+	<div class="mx-auto max-w-7xl px-6 lg:px-8">
+		<div class="mx-auto max-w-2xl lg:text-center">
+			<h2 id="Contact" class="text-3xl font-semibold leading-7">Contact Us</h2>
+		</div>
+		<div class="mx-auto mt-16 grid max-w-2xl sm:mt-20 lg:mx-0 lg:mt-24 lg:max-w-4xl lg:grid-cols-3">
+			<div class="flex flex-col">
+				<h2 id="Blog" class="text-3xl font-bold tracking-tight sm:text-4xl">Get in touch</h2>
+				<p class="mt-2 text-lg leading-8">We'd love to hear from you! Feel free to reach out.</p>
+			</div>
+			<div class="relative pl-16">
+				<dt class=" font-semibold leading-7">Email</dt>
+				<dd class="mt-2 leading-7">
+					<a href="mailto:Y7W9v@example.com" class="text-gray-900">Y7W9v@example.com</a>
+				</dd>
+			</div>
+			<div class="relative pl-16">
+				<dt class=" font-semibold leading-7">Phone</dt>
+				<dd class="mt-2 leading-7">+1 (555) 123-4567</dd>
+			</div>
+		</div>
+	</div>
+</div>
